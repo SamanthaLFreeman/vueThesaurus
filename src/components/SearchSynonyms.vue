78fc6fea7d625a5d121f7b5ced3b40f0da1923f9
Alan Turing <freemasl.sf@gmail.com>
2019-11-04 10:55:23 -0700

    Add basic setup for searchSynonyms form

---
+++ src/components/SearchSynonyms.vue
@@ -0,0 +1,20 @@
+<template>
+  <form>
+    <input type="text" name="synonym" placeholder="Search synonym" />
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SearchSynonyms",
+  data() {
+    return {
+      synonym: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
