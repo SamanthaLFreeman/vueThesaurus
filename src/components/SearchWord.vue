aefe18c1a83e05947af283bd69de9c33ca76e7ae
Alan Turing <freemasl.sf@gmail.com>
2019-11-04 22:23:16 -0700

    Center the form and change the font to courier

--- src/components/SearchWord.vue
+++ src/components/SearchWord.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit="search">
-    <input type="text" name="word" placeholder="Search word" v-model="word" />
+    <input type="text" name="word" placeholder="Search word..." v-model="word" />
     <button>Submit</button>
   </form>
 </template>
@@ -25,4 +25,22 @@ export default {
 </script>
 
 <style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  width: 400px;
+}
+
+input,
+button {
+  font-family: Courier;
+  font-size: 1em;
+  padding: 10px;
+}
+
+input {
+  margin-bottom: 10px;
+  text-align: center;
+}
 </style>
