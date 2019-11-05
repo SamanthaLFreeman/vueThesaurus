c769f43bd41e015aff2b4aa9fd9f338ee9259285
Alan Turing <freemasl.sf@gmail.com>
2019-11-04 20:46:00 -0700

    Remove child component and move button into WordsContainer so it calls fetch with the new word clicked

--- src/components/WordsContainer.vue
+++ src/components/WordsContainer.vue
@@ -1,21 +1,28 @@
 <template>
   <section>
-    <div v-bind:key="word.id" v-for="word in words">
-      <Word v-bind:word="word" />
-    </div>
+    <article class="word" v-bind:key="word.id" v-for="word in words">
+      <p>{{word.meta.id}}</p>
+      <p>{{word.fl}}</p>
+      <ul v-bind:key="def" v-for="def in word.shortdef">
+        <li>{{def}}</li>
+      </ul>
+      <div v-bind:key="synonym" v-for="synonym in word.meta.syns.flat()">
+        <button @click="$emit('display-word', synonym)">{{synonym}}</button>
+      </div>
+    </article>
   </section>
 </template>
 
 <script>
-import Word from "./Word";
 export default {
   name: "WordsContainer",
-  components: {
-    Word
-  },
   props: ["words"]
 };
 </script>
 
 <style scoped>
+.word {
+  padding: 20px;
+  text-align: center;
+}
 </style>
