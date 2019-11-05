24feeaf6c8fa66222ed5f22279113bc565752b86
Alan Turing <freemasl.sf@gmail.com>
2019-11-04 21:19:30 -0700

    Clean up data, so that each synonym listed under a word is unique

--- src/components/WordsContainer.vue
+++ src/components/WordsContainer.vue
@@ -1,12 +1,12 @@
 <template>
   <section>
-    <article class="word" v-bind:key="word.id" v-for="word in words">
-      <p>{{word.meta.id}}</p>
-      <p>{{word.fl}}</p>
-      <ul v-bind:key="def" v-for="def in word.shortdef">
+    <article class="word" v-bind:key="`${word.name}${word.type}`" v-for="word in words">
+      <p>{{word.name}}</p>
+      <p>{{word.type}}</p>
+      <ul v-bind:key="def" v-for="def in word.definition">
         <li>{{def}}</li>
       </ul>
-      <div v-bind:key="synonym" v-for="synonym in word.meta.syns.flat()">
+      <div v-bind:key="synonym" v-for="synonym in word.syns">
         <button @click="$emit('display-word', synonym)">{{synonym}}</button>
       </div>
     </article>
